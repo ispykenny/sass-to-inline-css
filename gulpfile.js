bd370b49762c443a8f5bc13638e84ea4060d4f97
Kenny Krosky <kenny.krosky@gmail.com>
2018-04-04 18:31:58 -0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const inlineCss = require('gulp-inline-css');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+gulp.task('serve', () => {
+	browserSync.init({
+		server: ''
+	})
+	gulp.watch('**/**/*.html' ,['inline']).on('change', browserSync.reload)	
+})
+
+gulp.task('scss', () => {
+	gulp.src('src/scss/**/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('build/css/'))
+	.pipe(browserSync.stream())
+})
+
+
+gulp.task('inline', () => {
+	gulp.src('./index.html')
+	.pipe(inlineCss({
+		removeHtmlSelectors: true
+	}))
+	.pipe(gulp.dest('build/'))
+});
+
+gulp.task('default', ['serve', 'scss'], () => {
+	gulp.watch('./src/scss/**/*.scss', ['scss','inline'])
+});
---
+++ readme.md
@@ -0,0 +1,12 @@
+# How To Use
+
+Clone the repository 
+
+enter into the file path
+
+run `npm install`
+
+to use the tool after it's been installed simply run
+`gulp`
+
+if you have any questions feel free to reach out on twitter [@ispykenny](https://twitter.com/ispykenny)
